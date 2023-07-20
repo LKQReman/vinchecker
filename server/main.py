39fceb1fa13926f6f4c083a12f8c2e391bc5ef34
Simeon Umoh <simeoneumoh@outlook.com>
2023-07-20 17:11:22 +0100

    updates

--- server/main.py
+++ server/main.py
@@ -28,18 +28,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
+
 @app.post("/result", response_class=HTMLResponse)
 async def process_vin(request: Request, vin: str = Form(...)):
-    client = Client('https://ws.vinlink.com/VLWS/services/Decoder?wsdl', wsse = UsernameToken(settings.USERNAME,settings.PASSWORD))
-    result = client.service.decode(vin,"BASIC")
+    client = Client('https://ws.vinlink.com/VLWS/services/Decoder?wsdl',
+                    wsse=UsernameToken(settings.USERNAME, settings.PASSWORD))
+    result = client.service.decode(vin, "BASIC")
     print(result)
     report = result[0]
 
@@ -47,7 +51,7 @@ async def process_vin(request: Request, vin: str = Form(...)):
     attributes = vinSpec.Item
 
     data = {
-        "request": request, 
+        "request": request,
         "vin": vin,
         "model_year": report.modelYear,
         "model_make": report.make,
