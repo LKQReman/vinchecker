e0c825303678f5f59a46edd6390fc667b5bd2481
Simeon Umoh <simeoneumoh@outlook.com>
2023-07-20 15:32:11 +0100

    template endpoint

---
+++ server/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+templates = Jinja2Templates(directory="templates")
+
+tags_metadata = []
+
+app = FastAPI(
+    title="Humdov",
+    description="Mobile",
+    openapi_tags=tags_metadata
+)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", tags=["Root"])
+async def read_root() -> dict:
+    return {"message": "Humdov App Backend service!"}
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+
